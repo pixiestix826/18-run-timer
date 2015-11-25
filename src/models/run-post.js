1fe88750b08a8f7b7c0cf265e41fd4cd5acc2d1b
Brandi Soucy <bbsoucy@gmail.com>
2015-11-24 22:54:34 -0500

    Models Collections Routes Map

---
+++ src/models/run-post.js
@@ -0,0 +1,10 @@
+export default Backbone.Model.extend({
+
+  urlRoot: 'http://tiny-lr.herokuapp.com/collections/runs-bbs',
+
+  defaults: {
+    time: '',
+    date: '',
+    notes: '',
+  },
+});
--- src/router.js
+++ src/router.js
@@ -1,5 +1,11 @@
-var Router = Backbone.Router.extend({
 
+var Router = Backbone.Router.extend({
+  routes: {
+    '': 'listAllRuns',
+    new: 'newRun',
+    ':id': 'runDetail',
+    ':id/edit': 'editRun',
+  },
 });
 
 export default Router;
