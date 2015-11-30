e5b54d1e5f9a87ff59c743ae75205c7ded60f3b9
Brandi Soucy <bbsoucy@gmail.com>
2015-11-29 18:30:13 -0600

    creating view for list item

--- src/views/posts-index.js
+++ src/views/posts-index.js
@@ -1,3 +1,22 @@
+var ListItem = Backbone.View.extend({
+  model: null,
+
+  tagName: 'li',
+
+  initialize() {
+    // Wire up model events
+  },
+  render () {
+    // Sets up the DOM
+    this.$el.html(`
+      ${this.model.get('date')} - ${this.model.get('time')}
+
+        <a href="#"><i class="fa fa-plus-circle"></i></i></a>
+        `);
+      return this.$el;
+    }
+});
+
 export default Backbone.View.extend({
   collection: null,
 
